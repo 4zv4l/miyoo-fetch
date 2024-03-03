ff7c4d1fee78998b54f8f8f575d2c7b6fd91486f
azz <4zv4l@protonmail.com>
2024-03-03 20:29:55 +0800

    code refactoring

--- miyoo-fetch.zig
+++ miyoo-fetch.zig
@@ -1,3 +1,6 @@
+// ram usage is around 1.5mb
+// 500kb    for the http request/response
+// 1mb      for the file download buffering
 const std = @import("std");
 const log = std.log;
 const fmt = std.fmt;
@@ -5,82 +8,61 @@ const http = std.http;
 const Allocator = std.mem.Allocator;
 const print = std.debug.print;
 
+// keep log.info even in ReleaseFast
+pub const std_options = .{ .log_level = .info };
+
 // get ID from user
-// free memory before returning
-fn getRomID(allocator: Allocator) !usize {
+fn getFileID() !usize {
     var stdin = std.io.getStdIn().reader();
-    var array = std.ArrayList(u8).init(allocator);
-    defer array.deinit();
-    try stdin.streamUntilDelimiter(array.writer(), '\n', 1024);
-    return try fmt.parseUnsigned(usize, array.items, 10);
+    var array: std.BoundedArray(u8, 128) = .{};
+    try stdin.streamUntilDelimiter(array.writer(), '\n', 128);
+    return try fmt.parseUnsigned(usize, array.buffer[0..array.len], 10);
 }
 
-// get the ROM name from the website using the ID
-// caller is the owner of the rom name (need to free)
-fn getRomName(allocator: Allocator, client: *http.Client, headers: *http.Headers, id: usize) ![]const u8 {
-    // format url
-    const url = try fmt.allocPrint(allocator, "https://edgeemu.net/details-{d}.htm", .{id});
-    log.info("Requesting rom's info...", .{});
-    defer allocator.free(url);
-    const uri = try std.Uri.parse(url);
-
-    // do ther request
-    var req = try client.request(.GET, uri, headers.*, .{});
+// download file from url as `filename`
+fn downloadFile(allocator: Allocator, client: *http.Client, uri: std.Uri, filename: ?[]const u8) !void {
+    // setup http request
+    const headers_buffer = try allocator.alloc(u8, 1024);
+    defer allocator.free(headers_buffer);
+    var req = try client.open(.GET, uri, .{ .server_header_buffer = headers_buffer });
     defer req.deinit();
-    try req.start();
+
+    // send http request and wait for the response
+    try req.send(.{});
     try req.wait();
 
-    var array = std.ArrayList(u8).init(allocator);
-    defer array.deinit();
+    // get file info
+    const file_size = req.response.content_length.?;
+    const file_name = blk: {
+        const content = req.response.content_disposition.?;
+        var it = std.mem.split(u8, content, "\"");
+        _ = it.next(); // skip first one
+        break :blk it.next().?;
+    };
 
-    log.info("Looking for ROM's name", .{});
-    while (req.reader().streamUntilDelimiter(array.writer(), '\n', 1024)) {
-        if (std.mem.startsWith(u8, array.items, "Name:  ")) {
-            const start = 7; // skip "Name:  "
-            const _end = std.mem.indexOf(u8, array.items, "<");
-            if (_end) |end| {
-                // if Rom's name is empty
-                if (start == end) return error.RomNotFound;
-                return try allocator.dupe(u8, array.items[start..end]);
-            }
-            return error.NameError;
-        }
-        try array.resize(0);
-    } else |_| {}
+    // log file info
+    log.info("filename: {s}", .{file_name});
+    log.info("Downloading {d}kb", .{file_size / 1024});
 
-    return error.RomNotFound;
-}
-
-// download rom by chunk of 4kb into `filename`
-fn downloadFile(allocator: Allocator, client: *http.Client, headers: *http.Headers, filename: []const u8, id: usize) !void {
-    // open destination file
-    var file = try std.fs.cwd().createFile(filename, .{});
+    // create target file (overwrite if already exists !)
+    const target_file = if (filename != null) filename.? else file_name;
+    var file = try std.fs.cwd().createFile(target_file, .{});
     defer file.close();
-    log.info("Opened destination file", .{});
 
-    // format url
-    const url = try fmt.allocPrint(allocator, "https://edgeemu.net/down.php?id={d}", .{id});
-    log.info("Requesting download...", .{});
-    defer allocator.free(url);
-    const uri = try std.Uri.parse(url);
-
-    var req = try client.request(.GET, uri, headers.*, .{});
-    defer req.deinit();
-    try req.start();
-    try req.wait();
-
-    log.info("Downloading...", .{});
-    var buff: [4096]u8 = undefined;
+    // download file by chunk if 1mb
+    var buff: [1024 * 1024]u8 = undefined;
+    var currently_read: usize = 0;
     while (true) {
+        print("\rinfo: Downloaded {d}/{d}kb", .{ currently_read / 1024, file_size / 1024 });
         const read = try req.readAll(&buff);
         _ = try file.writeAll(buff[0..read]);
+        currently_read += read;
         if (read < buff.len) break;
     }
-    log.info("Downloaded !", .{});
+    print("\rinfo: Downloaded {d}/{d}kb\n", .{ currently_read / 1024, file_size / 1024 });
 }
 
 pub fn main() !void {
-    // setup allocator
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     var allocator = gpa.allocator();
     defer _ = gpa.deinit();
@@ -89,20 +71,14 @@ pub fn main() !void {
     var client = http.Client{ .allocator = allocator };
     defer client.deinit();
 
-    // setup headers (None)
-    var headers = http.Headers{ .allocator = allocator };
-    defer headers.deinit();
+    print("Welcome !\nPlease enter the rom ID from edgeemu.net: ", .{});
 
-    print("Welcome ! Please enter the rom ID from edgeemu.net:\n", .{});
+    // get file ID from user and setup the target uri for download
+    const id = try getFileID();
+    const url = try fmt.allocPrint(allocator, "https://edgeemu.net/down.php?id={d}", .{id});
+    defer allocator.free(url);
+    const uri = try std.Uri.parse(url);
 
-    const id = try getRomID(allocator);
-    const name = try getRomName(allocator, &client, &headers, id);
-    defer allocator.free(name);
-    const filename = try fmt.allocPrint(allocator, "{s}.7z", .{name});
-    defer allocator.free(filename);
-
-    log.info("Rom's id: {d} !", .{id});
-    log.info("Rom's name: {s} !", .{name});
-
-    try downloadFile(allocator, &client, &headers, filename, id);
+    try downloadFile(allocator, &client, uri, null);
+    log.info("Thanks for using me :)", .{});
 }
